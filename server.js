66ef4763725411ccf2cbe0073d7377b863ce160a
renaud devers <rdevers@chavers.org>
2018-08-16 13:47:25 +0200

    switch https to http if certificat not found

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 'use strict'
 process.env.NODE_ENV = 'production';
+var fs = require('fs');
+var path = require('path');
+var confFile = path.resolve(__dirname, 'config.json');
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
-var server = require('http').createServer(app)
+var http = require('http');
+var https = require('https');
+var usehttps = false;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var bcrypt = require('bcryptjs');
 var mcache = require('memory-cache');
 
-var config = require('./config.json');
-// var r = require('./tools/register');
-// r(config);
+var config = require(confFile);
+var externalkey = path.resolve(__dirname, config.externalkey);
+var externalcert = path.resolve(__dirname, config.externalcert);
+var privateKey;
+var certificate;
+var credentials;
+
+try {
+  fs.statSync(externalkey).isFile();
+  fs.statSync(externalcert).isFile();
+  privateKey = fs.readFileSync(externalkey, 'utf8');
+  usehttps = true;
+  certificate = fs.readFileSync(externalcert, 'utf8');
+  credentials = { key: privateKey, cert: certificate };
+} catch (error) {
+  console.log('No certificat, use http');
+}
+
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -52,8 +72,11 @@ var accessRoutes = require('./api/routes/accessRoutes');
 accessRoutes(app);
 var renewRoutes = require('./api/routes/renewRoutes');
 renewRoutes(app);
-
-
+if (usehttps) {
+  var server = https.createServer(credentials, app);
+} else {
+  var server = http.createServer(app);
+}
 var port = config.port || 5000
 server.listen(port, function () {
   console.log('ezb_sta server listening on port %d in %s mode', port, app.get('env'))
